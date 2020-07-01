629471ff69055b98b719be1e5862b3aa1ae6eac7
조장우 <jopuk2@gmail.com>
2020-07-01 20:15:34 +0900

    주석추가

--- app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
+++ app/src/main/java/kr/co/tjoeun/kotlinbasic_20200701/MainActivity.kt
@@ -15,7 +15,8 @@ class MainActivity : AppCompatActivity() {
         // touchBtn이 눌리면 실행될 코드를 적는 {}
 
             //Toast를 이용해 사용자에게 안내 메세지.
-            Toast.makeText(this, "터치버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
+//            Toast.makeText(this, "터치버튼을 눌렀습니다.", Toast.LENGTH_SHORT).show()
+            Toast.makeText(this, "터치버튼을 눌렀습니다.", Toast.LENGTH_LONG).show()
         }
     }
 }
